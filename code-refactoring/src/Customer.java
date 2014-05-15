b97b684ced407b3d850773ed41c6d100eb2da520
Th3Tw0 <thetwo@gmx.net>
2014-05-15 21:03:50 +0200

    Renamed ammountFor's parameter Rental rental to aRental. Renamed thisAmmount in result (ammountFor)

--- code-refactoring/src/Customer.java
+++ code-refactoring/src/Customer.java
@@ -41,24 +41,24 @@ class Customer {
         return result;
     }
     
-	private double ammountFor(Rental rental) {
-		double amount = 0;
-		switch (rental.getMovie().getPriceCode()) {
+	private double ammountFor(Rental aRental) {
+		double result = 0;
+		switch (aRental.getMovie().getPriceCode()) {
 		    case Movie.REGULAR:
-		    	amount += 2;
-		    	if (rental.getDaysRented() > 2)
-		        	amount += (rental.getDaysRented() - 2) * 1.5;
+		    	result += 2;
+		    	if (aRental.getDaysRented() > 2)
+		        	result += (aRental.getDaysRented() - 2) * 1.5;
 		    	break;
 		    case Movie.NEW_RELEASE:
-		    	amount += rental.getDaysRented() * 3;
+		    	result += aRental.getDaysRented() * 3;
 		    	break;
 		    case Movie.CHILDRENS:
-		    	amount += 1.5;
-		    	if (rental.getDaysRented() > 3)
-		        	amount += (rental.getDaysRented() - 3) * 1.5;
+		    	result += 1.5;
+		    	if (aRental.getDaysRented() > 3)
+		        	result += (aRental.getDaysRented() - 3) * 1.5;
 		    	break;
 		}
-		return amount;
+		return result;
 	}
     
 }
