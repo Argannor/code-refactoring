9593cdf0bfc342aff9ebf3a88fa3bb00ed32ca21
Th3Tw0 <thetwo@gmx.net>
2014-05-15 21:01:25 +0200

    Extracted the switch statement in the statement() method to the new method double ammountFor(Rental)

--- code-refactoring/src/Customer.java
+++ code-refactoring/src/Customer.java
@@ -25,21 +25,7 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                	thisAmount += 2;
-                	if (each.getDaysRented() > 2)
-                    	thisAmount += (each.getDaysRented() - 2) * 1.5;
-                	break;
-                case Movie.NEW_RELEASE:
-                	thisAmount += each.getDaysRented() * 3;
-                	break;
-                case Movie.CHILDRENS:
-                	thisAmount += 1.5;
-                	if (each.getDaysRented() > 3)
-                    	thisAmount += (each.getDaysRented() - 3) * 1.5;
-                	break;
-            }
+            thisAmount = ammountFor(each);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -54,6 +40,26 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+    
+	private double ammountFor(Rental rental) {
+		double amount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		    	amount += 2;
+		    	if (rental.getDaysRented() > 2)
+		        	amount += (rental.getDaysRented() - 2) * 1.5;
+		    	break;
+		    case Movie.NEW_RELEASE:
+		    	amount += rental.getDaysRented() * 3;
+		    	break;
+		    case Movie.CHILDRENS:
+		    	amount += 1.5;
+		    	if (rental.getDaysRented() > 3)
+		        	amount += (rental.getDaysRented() - 3) * 1.5;
+		    	break;
+		}
+		return amount;
+	}
     
 }
     
